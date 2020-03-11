d5c225c87d5dfe5adeb1c5916151d8086ffaecb9
olebaba <olebastiank.lochen@gmail.com>
2020-03-11 22:41:23 +0100

    Consumer added

---
+++ src/main/java/Classes/Consumer.java
@@ -0,0 +1,40 @@
+package Classes;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.LongDeserializer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.Duration;
+import java.util.Collections;
+import java.util.Properties;
+
+public class Consumer{
+    public static void main(String[] args) {
+        Logger logger = LoggerFactory.getLogger(Consumer.class.getName());
+
+        String topic = "ranking_element_count";
+
+        Properties props = new Properties();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG,   StringDeserializer.class.getName());
+        props.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, LongDeserializer.class.getName());
+        props.setProperty(ConsumerConfig.GROUP_ID_CONFIG, "test");
+
+        KafkaConsumer<String, Long> kafkaConsumer = new KafkaConsumer<>(props);
+
+        kafkaConsumer.subscribe(Collections.singletonList(topic));
+
+        while (true){
+            ConsumerRecords<String, Long> records = kafkaConsumer.poll(Duration.ofMillis(100));
+            for (ConsumerRecord<String, Long> record: records){
+                System.out.println("Key: " + record.key() + ", Value: " + record.value());
+                System.out.println("Partition: " + record.partition() + ", Offset: " + record.offset());
+            }
+        }
+    }
+}
--- src/main/java/myapps/ChoiceCounter.java
+++ src/main/java/myapps/ChoiceCounter.java
@@ -2,6 +2,8 @@ package myapps;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.common.serialization.Deserializer;
 import org.apache.kafka.common.serialization.Serde;
 import org.apache.kafka.common.serialization.Serdes;
@@ -19,64 +21,78 @@ import org.apache.kafka.streams.kstream.Materialized;
 import org.apache.kafka.streams.kstream.Produced;
 import org.apache.kafka.streams.state.KeyValueStore;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import java.util.Properties;
 import java.util.concurrent.CountDownLatch;
 
 public class ChoiceCounter {
 
     public static void main(String[] args) throws Exception {
-        Properties props = new Properties();
-        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "streams-choice-counter");
-        props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-        props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
-        props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
-
-        final StreamsBuilder builder = new StreamsBuilder();
-
-        final Serializer<JsonNode> nodeSerializer = new JsonSerializer();
-        final Deserializer<JsonNode> nodeDeserializer = new JsonDeserializer();
-        final Serde<JsonNode> jsonNodeSerde = Serdes.serdeFrom(nodeSerializer, nodeDeserializer);
-
-        String topic = "user-input";//"ranking_element_choices";
-
-        KStream<String, JsonNode> rankingSource = builder
-                .stream(topic, Consumed.with(Serdes.String(), jsonNodeSerde));
-
-        //rankingSource.to("ranking_element_count"); //Pipe
-
-        //ObjectMapper om = new ObjectMapper();
-
-        KStream<String, Long> rankingCount = rankingSource
-                .flatMapValues((s, jsonNode) -> jsonNode.get("user_id"))
-                .groupBy((s, jsonNode) -> jsonNode.get("user_id").toString())
-                .count(Materialized.as("counting-store"))
-                .toStream();
-
-        rankingSource.foreach((s, jsonNode) -> {
-            System.out.println(jsonNode.toPrettyString());
-        });
-
-        rankingCount.to("ranking_element_count", Produced.with(Serdes.String(), Serdes.Long()));
-
-
-        final Topology topology = builder.build();
-        final KafkaStreams streams = new KafkaStreams(topology, props);
-        final CountDownLatch latch = new CountDownLatch(1);
-
-        Runtime.getRuntime().addShutdownHook(new Thread("streams-shutdown-hook"){
-            @Override
-            public void run() {
-                streams.close();
-                latch.countDown();
-            }
-        });
-
         try {
-            streams.start();
-            latch.await();
-        }catch (Throwable e){
-            System.exit(1);
+
+
+            Properties props = new Properties();
+            props.put(StreamsConfig.APPLICATION_ID_CONFIG, "streams-choice-counter");
+            props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+            props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+            props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.Long().getClass());
+
+            final StreamsBuilder builder = new StreamsBuilder();
+
+            final Serializer<JsonNode> nodeSerializer = new JsonSerializer();
+            final Deserializer<JsonNode> nodeDeserializer = new JsonDeserializer();
+            final Serde<JsonNode> jsonNodeSerde = Serdes.serdeFrom(nodeSerializer, nodeDeserializer);
+
+            String topic = "user-input3";//"ranking_element_choices";
+
+
+            KStream<String, JsonNode> rankingSource = builder
+                    .stream(topic, Consumed.with(Serdes.String(), jsonNodeSerde));
+
+            //rankingSource.to("ranking_element_count"); //Pipe
+
+            //ObjectMapper om = new ObjectMapper();
+
+            KStream<String, Long> rankingCount = rankingSource
+                    .filter((s, jsonNode) -> jsonNode != null)
+                    .groupBy((s, jsonNode) -> jsonNode.get("user_id").textValue())
+                    .count(Materialized.as("counting-store"))
+                    .toStream();
+
+            rankingSource.foreach((s, jsonNode) -> {
+                System.out.println(jsonNode.toPrettyString());
+            });
+
+            //
+            // Consumer<Long, String> consumer = new KafkaConsumer<>(props);
+            //consumer.subscribe(Collections.singleton("ranking_element_count"));
+
+            rankingCount.to("ranking_element_count", Produced.with(Serdes.String(), Serdes.Long()));
+
+
+            final Topology topology = builder.build();
+            final KafkaStreams streams = new KafkaStreams(topology, props);
+            final CountDownLatch latch = new CountDownLatch(1);
+
+            Runtime.getRuntime().addShutdownHook(new Thread("streams-shutdown-hook") {
+                @Override
+                public void run() {
+                    streams.close();
+                    latch.countDown();
+                }
+            });
+
+            try {
+                streams.start();
+                latch.await();
+            } catch (Throwable e) {
+                System.exit(1);
+            }
+            System.exit(0);
+        }catch (Exception e){
+            System.out.println(Arrays.toString(e.getStackTrace()));
         }
-        System.exit(0);
     }
 }
